9e9857e39c0661d8e71027114d3119ee304fe306
tomaszdurka <tomasz@durka.pl>
2014-02-11 01:39:07 +0100

    Store filesystem helper instead of output

--- library/CMTools/Generator/Application.php
+++ library/CMTools/Generator/Application.php
@@ -5,8 +5,8 @@ class CMTools_Generator_Application extends CM_Class_Abstract {
 	/** @var CM_App_Installation */
 	private $_installation;
 
-	/** @var CM_OutputStream_Abstract */
-	private $_output;
+	/** @var CMTools_Generator_FilesystemHelper */
+	private $_filesystemHelper;
 
 	/**
 	 * @param CM_App_Installation      $appInstallation
@@ -14,7 +14,7 @@ class CMTools_Generator_Application extends CM_Class_Abstract {
 	 */
 	public function __construct(CM_App_Installation $appInstallation, CM_OutputStream_Abstract $output) {
 		$this->_installation = $appInstallation;
-		$this->_output = $output;
+		$this->_filesystemHelper = new CMTools_Generator_FilesystemHelper($output);
 	}
 
 	/**
@@ -22,8 +22,7 @@ class CMTools_Generator_Application extends CM_Class_Abstract {
 	 * @param string $path
 	 */
 	public function addModule($name, $path) {
-		$filesystemHelper = new CMTools_Generator_FilesystemHelper($this->_output);
-		$filesystemHelper->createDirectory(DIR_ROOT . $path);
+		$this->_filesystemHelper->createDirectory(DIR_ROOT . $path);
 		$configAdditions = array(
 				'extra' => array(
 						'cm-modules' => array(
@@ -43,8 +42,7 @@ class CMTools_Generator_Application extends CM_Class_Abstract {
 		$composerFile = new Composer\Json\JsonFile(DIR_ROOT . 'composer.json');
 		$configCurrent = $composerFile->read();
 
-		$filesystemHelper = new CMTools_Generator_FilesystemHelper($this->_output);
-		$filesystemHelper->notify('modify', $composerFile->getPath());
+		$this->_filesystemHelper->notify('modify', $composerFile->getPath());
 
 		$configMerged = array_merge_recursive($configCurrent, $hash);
 		$composerFile->write($configMerged);
