b60c79e9d1fb353ba609230292be2c710fe69049
Chris Stasiak <me@krzysztof-stasiak.com>
2014-02-11 16:04:03 +0100

    Added create-class and create-view tests

--- features/Generator/CreateClass.feature
+++ features/Generator/CreateClass.feature
@@ -2,7 +2,22 @@ Feature: generator create-class
   It should create new classes.
 
 Scenario: Create a model class
-  Given file "library/CMTools/Model/Foo.php" does not exist
-  When I run "scripts/cm.php generator create-class CMTools_Model_Foo"
-  Then file "library/CMTools/Model/Foo.php" exists
+  Given file "library/CMTools/Module/Foo.php" does not exist
+  When I run "scripts/cm.php generator create-class CMTools_Module_Foo"
+  Then file "library/CMTools/Module/Foo.php" exists
 
+Scenario: Create a class
+  Given file "library/CMTools/Foo/Bar.php" does not exist
+  When I run "scripts/cm.php generator create-class CMTools_Foo_Bar"
+  Then file "library/CMTools/Foo/Bar.php" exists
+
+Scenario: Create a view
+  Given file "library/CMTools/Foo/Bar.js" does not exist
+  And file "library/CMTools/Foo/Bar.php" does not exist
+  And file "layout/default/Foo/Bar/default.less" does not exist
+  And file "layout/default/Foo/Bar/default.tpl" does not exist
+  When I run "scripts/cm.php generator create-view CMTools_Foo_Bar"
+  Then file "library/CMTools/Foo/Bar.js" exists
+  And file "library/CMTools/Foo/Bar.php" exists
+  And file "layout/default/Foo/Bar/default.less" exists
+  And file "layout/default/Foo/Bar/default.tpl" exists
--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,74 +1,35 @@
 <?php
 
-use Behat\Behat\Context\ClosuredContextInterface,
-    Behat\Behat\Context\TranslatedContextInterface,
-    Behat\Behat\Context\BehatContext,
-    Behat\Behat\Exception\PendingException;
-use Behat\Gherkin\Node\PyStringNode,
-    Behat\Gherkin\Node\TableNode;
-
-//
-// Require 3rd-party libraries here:
-//
-//   require_once 'PHPUnit/Autoload.php';
-//   require_once 'PHPUnit/Framework/Assert/Functions.php';
-//
+use Behat\Behat\Context\BehatContext;
 
 /**
  * Features context.
  */
-class FeatureContext extends BehatContext
-{
-    /**
-     * Initializes context.
-     * Every scenario gets it's own context object.
-     *
-     * @param array $parameters context parameters (set them up through behat.yml)
-     */
-    public function __construct(array $parameters)
-    {
-        // Initialize your context here
-    }
+class FeatureContext extends BehatContext {
+	/**
+	 * @Given /^file "([^"]*)" does not exist$/
+	 */
+	public function fileDoesNotExist($file) {
+		if (CM_File::exists($file)) {
+			$file = new CM_File($file);
+			$file->delete();
+		}
+	}
 
-//
-// Place your definition and hook methods here:
-//
-//    /**
-//     * @Given /^I have done something with "([^"]*)"$/
-//     */
-//    public function iHaveDoneSomethingWith($argument)
-//    {
-//        doSomethingWith($argument);
-//    }//
+	/**
+	 * @Then /^file "([^"]*)" exists$/
+	 */
+	public function fileExists($file) {
+		if (!CM_File::exists($file)) {
+			throw new Exception("File does not exist!");
+		}
+	}
 
-    /**
-     * @Given /^file "([^"]*)" does not exist$/
-     */
-    public function fileDoesNotExist($file)
-    {
-        if(CM_File::exists($file)) {
-           $file = new CM_File($file);
-            $file->delete();
-        }
-    }
-
-    /**
-     * @Then /^file "([^"]*)" exists$/
-     */
-    public function fileExists($file)
-    {
-        if(!CM_File::exists($file)) {
-            throw new Exception("File does not exist!");
-        }
-    }
-
-    /**
-     * @When /^I run "([^"]*)"$/
-     */
-    public function iRun($command)
-    {
-        CM_Util::exec($command);
-    }
-
+	/**
+	 * @When /^I run "([^"]*)"$/
+	 */
+	public function iRun($command) {
+		CM_Util::exec($command);
+	}
 }
 
